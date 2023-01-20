fd485a7708ccad2c3bd3ed8ae3cf95d3454bb579
21264 <21264@HUAWEI>
2023-01-20 21:30:08 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+public class Biblio
+{ }
+public class Livre
+
+{
+    private class Livrevoulu
+    {
+
+
+        string lebon;
+        lebon = None;
+            public class Emprunt
+        {
+            class research
+            {
+            lebon==trouve;
+                }
+    }
+    }
+    private String NomLivre;
+    private int IDLivre;
+
+
+}
+
+
+
+
+public class Emprunt
+{
+    private class delay
+    {
+        int tempsdonne;
+        int repet;
+        if (tempdonne > 5 || repet>=5)
+            { return "Retour contentieux du livre"
+            }
+        else {
+            return Emprunt
+            }
+    }
+    private class etatlivre
+    {
+        string etat;
+        if (etatlivre==mauvais)
+            {
+            return "Contentieux allégeants"
+            }
+        else
+            {
+            return Emprunt
+            
+            }
+    }
+}
+
+
+
+
+public class Adherent
+{
+    private String Nom;
+    private int CNE;
+    private string Prenom;
+}
